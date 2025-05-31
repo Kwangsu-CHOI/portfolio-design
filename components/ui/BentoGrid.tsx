742af219b682b499e914aa31d430aa508c464e0a
Kwangsu-CHOI <wisecks@gmail.com>
2025-05-31 18:32:02 +1000

    minor update

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -125,11 +125,6 @@ export const BentoGridItem = ({
           )}
         >
           {/* change the order of the title and des, font-extralight, remove text-xs text-neutral-600 dark:text-neutral-300 , change the text-color */}
-          <div className="font-sans font-extralight md:max-w-70 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10 ">
-            {description}
-          </div>
-          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
-          {/* remove mb-2 mt-2 */}
           <div
             className={`font-sans text-lg lg:text-3xl max-w-96 font-bold z-10 w-[24rem] ${
               id === 6 &&
@@ -138,6 +133,11 @@ export const BentoGridItem = ({
           >
             {title}
           </div>
+          <div className="font-sans font-extralight md:max-w-70 md:text-xs lg:text-base text-sm text-[#C1C2D3] z-10 mt-2 md:mt-4">
+            {description}
+          </div>
+          {/* add text-3xl max-w-96 , remove text-neutral-600 dark:text-neutral-300*/}
+          {/* remove mb-2 mt-2 */}
 
           {/* for the github 3d globe */}
           {id === 2 && <GridGlobe />}
